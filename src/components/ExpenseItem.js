20f4c2d75f60ae80ed1232835ab76a1c5af0c427
liuchongxi <jacky-liu9@hotmail.com>
2021-08-18 22:30:46 -0700

    Imporve ExpenseItem component, add style.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>Aug. 21th 2021</div>
+      <div className="expense-item__description">
+        <h2>Food</h2>
+        <div className="expense-item__price">$25</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
