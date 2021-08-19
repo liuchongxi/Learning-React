0e625fd531cf70a06fe1726b819a03ed2bdf6a0f
liuchongxi <jacky-liu9@hotmail.com>
2021-08-19 00:35:40 -0700

    Formate expense date

--- src/ExpenseTracker/components/ExpenseItem.js
+++ src/ExpenseTracker/components/ExpenseItem.js
@@ -2,12 +2,20 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const expenseDate = props.date;
+  const month = expenseDate.toLocaleString("cn", { month: "long" });
+  const year = expenseDate.toLocaleString("cn", { day: "2-digit" });
+  const day = expenseDate.getFullYear();
+
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
