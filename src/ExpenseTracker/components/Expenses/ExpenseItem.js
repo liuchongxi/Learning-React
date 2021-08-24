a716f7d3886edaea8f0d86fb5f1c89f1f1bcaf15
liuchongxi <jacky-liu9@hotmail.com>
2021-08-24 01:32:00 -0700

    play around with state

--- src/ExpenseTracker/components/Expenses/ExpenseItem.js
+++ src/ExpenseTracker/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../Utils/Card/Card";
 
 function ExpenseItem(props) {
-  const expenseTitle = props.title;
+  const [expenseTitle, setExpenseTitle] = useState(props.title);
   const expenseAmount = props.amount;
+
+  const ChangeTitleHandler = () => {
+    console.log("clicked.");
+    setExpenseTitle("new title after clicked");
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
+        <button onClick={ChangeTitleHandler}>Change title</button>
       </div>
     </Card>
   );
